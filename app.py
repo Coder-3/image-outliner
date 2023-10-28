9dcaeb3c79b5b3a06096a6a0e8ca80ebf53b7e11
Cedric <cedric@liquid-state.com>
2023-10-28 22:16:29 +1100

    Check if uploads directory exists, if not, create it.

--- app.py
+++ app.py
@@ -8,15 +8,18 @@ UPLOAD_FOLDER = 'uploads/'
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        if not os.path.exists(UPLOAD_FOLDER):
+            os.makedirs(UPLOAD_FOLDER)
+        
         file = request.files['file']
         lower_threshold = int(request.form.get('lower_threshold', 50))
         upper_threshold = int(request.form.get('upper_threshold', 150))
         filename = os.path.join(UPLOAD_FOLDER, file.filename)
         file.save(filename)
-        # Your conversion function here
         converted_image_path = convert_image_to_non_transparent_outline(filename, lower_threshold, upper_threshold)
         return send_from_directory(directory=os.getcwd(), filename=converted_image_path)
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
